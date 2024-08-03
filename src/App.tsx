0d010ef89a6d6be45c807da33cb56c084c6a5d0d
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-08-04 02:35:28 +0300

    feat: created the chatui component and add translation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import ChatUI from "./components/ChatUI";
+
+const App = () => {
+  return (
+    <div className="app">
+      <ChatUI />
+    </div>
+  );
+};
+
+export default App;
